3ef282a7fbcea3d921dd41632e48737b6ba51181
didinurrahmad <didirahmad24@gmail.com>
2023-11-01 16:21:06 +0800

    user

---
+++ layout/footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h2>Tentang Kami</h2>
+                <p>Kami adalah perusahaan yang bergerak di bidang...</p>
+            </div>
+            <div class="footer-section">
+                <h2>Alamat</h2>
+                <p>Jalan Pramuka 10<br>Kota Samarinda<br>75155</p>
+            </div>
+            <div class="footer-section">
+                <h2>Kontak</h2>
+                <p>Email: info@gmail.com<br>Telepon: 082123511416</p>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright">
+        Copyright &copy; 2023 | Kelompok 4 UAS Web-Lanjut
+    </div>
+</body>
+
+</html>
--- layout/header.php
+++ layout/header.php
@@ -8,25 +8,23 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap" rel="stylesheet">
-    <title>HOME</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
+    <title>Document</title>
 </head>
 
 <body>
-    <?php
-    include("layout/header.php");
-    ?>
-    <main>
-        <div class="content-left">
-            <h1>PRAMUKA FUTSAL</h1>
-            <h3>Lapangan Futsal Jalan Pramuka</h3>
-            <button class="btn-more"><a href="#">Selengkapnya</a></button>
+    <header>
+        <div class="logo">
+            <img src="img/logo.png " alt="" width="70px">
         </div>
-        <div class="content-right">
-            <img class="gambar-content" src="img/bola.png" alt="">
-        </div>
-    </main>
-
+        <nav>
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Kontak</a></li>
+                <li><a href="#">Lapangan</a></li>
+                <li><a href="#" class="signup-btn">Daftar Akun</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 
 </html>
